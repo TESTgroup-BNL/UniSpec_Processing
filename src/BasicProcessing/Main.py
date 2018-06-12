641442fa6717294d41457f15369bc06f9e453590
Andrew McMahon <amcmahon@bnl.gov>
2018-06-12 19:53:50 -0400

    2018 Improvement Testing
    First attempt at WP averaging, switchover and interpolation.  Also added date/time to output file.

--- src/BasicProcessing/Main.py
+++ src/BasicProcessing/Main.py
@@ -19,12 +19,13 @@ def main():
     
     """
 
-    path = str(os.path.realpath('.'))
+#    path = str(os.path.realpath('.'))
     # Edited by SPS on 11/06/2015
-    #Spec = UnispecProcessing(path + r'\config.txt')
-    Spec = UnispecProcessing(os.path.join(path, "config.txt"))
+    #spec = UnispecProcessing(path + r'\config.txt')
+#    spec = UnispecProcessing(os.path.join(path, "config.txt"))
+    spec = UnispecProcessing(os.path.join("src", "config.txt"))
     
-    run_count, WP_count, stop_count = Spec.GetFileLists()
+    run_count, WP_count, stop_count = spec.getFileLists()
     
     for run in range(0,run_count):
         
@@ -33,19 +34,21 @@ def main():
     
         WP_data = [[[None], [None]] for item in range(0,WP_count[run])]
         Stop_data = [[[None], [None]] for item in range(0,stop_count[run])]
+        WP_end_data = [[[None], [None]] for item in range(0,stop_count[run])]
         sat = [[None], [None]]
         
         
         #When getting data from these, they are formatted as:
         #    var[file index][header/data][row index][CH_B_WL/CH_B/CH_A_WL/CH_A]
-        WP_data = Spec.ReadFiles(Spec.WPs[run], Spec.HeaderLines)
-        Stop_data = Spec.ReadFiles(Spec.Stops[run], Spec.HeaderLines)
+        WP_data = spec.readFiles(spec.WPs[run], spec.HeaderLines)
+        Stop_data = spec.readFiles(spec.Stops[run], spec.HeaderLines)
+        WP_end_data = spec.readFiles(spec.WP_end[run], spec.HeaderLines)
         
         #Formatted as:
         #    var[file index][CH_B/CH_A][WL]
-        #    value of var is the WL saturation occurred at
-        sat_WP = Spec.CheckSaturation(WP_data)
-        sat_stops = Spec.CheckSaturation(Stop_data)
+        #    value is the WL saturation occurred at
+        sat_WP = spec.checkSaturation(WP_data)
+        sat_stops = spec.checkSaturation(Stop_data)
         
         print("Saturated Measurement Count\n\t\tCh_B\tCh_A")
         for idx, curfile in enumerate(sat_WP):
@@ -54,28 +57,34 @@ def main():
             print("Stop " + str(idx) + ":\t\t" + str(curfile[1]) + "\t" + str(curfile[2]))        
         print("\n" + str(len(sat_WP)) + " WPs and " + str(len(sat_stops)) + " stops saturated.")
         
-        #Spec.RemoveSaturated(WP_data, sat_WP)
-        #Spec.RemoveSaturated(Stop_data, sat_stops)
+        #spec.RemoveSaturated(WP_data, sat_WP)
+        #spec.RemoveSaturated(Stop_data, sat_stops)
+        
+        #Subtract dark current values
+        darkData = spec.readFiles([spec.darkSource], spec.darkHeaderLines)
+        WP_data_darkCorrected = spec.subtractDarkCurrent(WP_data, darkData, spec.swapChannels)
+        Stop_data_darkCorrected = spec.subtractDarkCurrent(Stop_data, darkData, spec.swapChannels)
         
         #Formatted as:
         #    var[file, WL/CH_B/CH_A] = [1 dim array of values]
-        intdata_WPs = Spec.Interp(WP_data)
-        intdata_Stops = Spec.Interp(Stop_data)
+        intdata_WPs = spec.interp(WP_data_darkCorrected)
+        intdata_Stops = spec.interp(Stop_data_darkCorrected)
         
-        avg_WP = Spec.AvgWPs(intdata_WPs)
+        avg_WP_start = spec.avgWPs(intdata_WPs)
+        WP_array = create_WP_array(avg_WP_start,WP_end_data,Stop_data)
         
         #Plot all WPs with average
-        #Spec.plot_Averaging(intdata_WPs, avg_WP)
+        #spec.plot_Averaging(intdata_WPs, avg_WP)
         
-        R = Spec.Refl(intdata_Stops, avg_WP)
+        R = spec.refl(intdata_Stops, WP_array)
         
         #Plot reflectance for a particular stop
         #    plot_R_A(Refl data, Stop #)
-        #Spec.plot_R(R,20)
+        #spec.plot_R(R,20)
         
-        dt = Spec.GetDateTime(WP_data[0])
+        dt = spec.getDateTime(WP_data[0])
         
-        Spec.WriteOutput(R, Spec.OutputPath, Spec.OutputPrefix + dt[consts.date] + "__" + dt[consts.time].replace(':','_') + ".csv")
+        spec.writeOutput(R, spec.OutputPath, spec.OutputPrefix + dt.strftime("%Y-%m-%d__%H:%M:%S") + ".csv", Stop_data)
         
         
 if __name__ == "__main__":
